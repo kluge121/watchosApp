2d8d516c2ec696e06495044450903fcdbbe583ab
benny <kluge0221@gmail.com>
2023-02-26 19:02:26 +0900

    Initial Commit

---
+++ watchosApp Watch App/watchosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchosAppApp.swift
+//  watchosApp Watch App
+//
+//  Created by 배민수 on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct watchosApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
